282c28e524e1d6c0c28a76b2b3e41a173f02b499
PechimuthuMithil <pechimuthumithil@iitgn.ac.in>
2024-01-20 15:52:54 +0530

    Deployment.py updated

--- Mini-Project/Deployement.py
+++ Mini-Project/Deployement.py
@@ -10,11 +10,12 @@ classes = {"Walking":1,"WalkingUpstairs":2,"WalkingDownstairs":3,"Sitting":4,"St
 
 X_test_collected = []
 y_test_collected = []
+print("Training the recognizer...")
 for folder in classes.keys():
     files = os.listdir("..\\Collected-Data\\"+folder)
     for file in files:
         path = "..\\Collected-Data\\" + folder + "\\" + file
-        print(path)
+        # print(path)
         try: 
             df = pd.read_excel(path)
         except:
@@ -26,7 +27,6 @@ for folder in classes.keys():
         y_test_collected.append(classes[folder])
 
 X_test_collected = np.array(X_test_collected)
-print(X_test_collected.shape)
 
 X_train = np.load('X_train.npy')
 y_train = np.load('y_train.npy')
@@ -36,11 +36,11 @@ X_val = np.load('X_val.npy')
 y_val = np.load('y_val.npy')
 
 X_data = []
-# for i in range(len(X_test)):
-#     temp = []
-#     for j in range(len(X_test[0])):
-#         temp.append(np.dot(X_test[i][j],np.transpose(X_test[i][j])))
-#     X_data.append(temp)
+for i in range(len(X_test)):
+    temp = []
+    for j in range(len(X_test[0])):
+        temp.append(np.dot(X_test[i][j],np.transpose(X_test[i][j])))
+    X_data.append(temp)
 
 for i in range(len(X_train)):
     temp = []
@@ -55,8 +55,8 @@ for i in range(len(X_val)):
     X_data.append(temp)
 
 X_data = np.array(X_data)
-# y_data = np.array(list(y_test)+list(y_train)+list(y_val))
-y_data = np.array(list(y_train)+list(y_val))
+y_data = np.array(list(y_test)+list(y_train)+list(y_val))
+# y_data = np.array(list(y_train)+list(y_val))
 X_train, X_val, y_train, y_val = train_test_split(X_data, y_data, test_size=0.3, random_state=42)
 
 Recognizer = tree.DecisionTreeClassifier(max_depth=7, min_samples_split=5, criterion='gini', random_state=42)
